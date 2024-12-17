a7051c3e0e0d861f13cfe0aae2bef9883d534e32
Argyn <argynakhmetov3@gmail.com>
2024-12-17 18:22:49 +0500

    first commit

--- app/urls.py
+++ app/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('user/', include('users.urls', namespace='user')),
     path('cart/', include('carts.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('', include('paypal.standard.ipn.urls')),
 ]
 
 if settings.DEBUG:
